eb7c72ff15b740d6f01d7a29b099a261f47512de
teuku m saif <95566044+Saip-kun@users.noreply.github.com>
2023-04-13 11:00:03 +0700

    Update SJF.java

--- SJF.java
+++ SJF.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 public class SJF {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+
         System.out.print("Masukkan jumlah proses: ");
         int n = input.nextInt();
-        
+
         int[] arrivalTime = new int[n];
         int[] burstTime = new int[n];
         int[] completionTime = new int[n];
         int[] turnAroundTime = new int[n];
         int[] waitingTime = new int[n];
         boolean[] processed = new boolean[n];
-        
+
         for (int i = 0; i < n; i++) {
-            System.out.println("Masukkan data untuk proses " + (i+1) + ":");
+            System.out.println("Masukkan data untuk proses " + (i + 1) + ":");
             System.out.print("  - Waktu kedatangan: ");
             arrivalTime[i] = input.nextInt();
             System.out.print("  - Waktu burst: ");
             burstTime[i] = input.nextInt();
             processed[i] = false;
         }
-        
+
         int currentTime = 0;
         int completed = 0;
         while (completed != n) {
@@ -45,19 +45,27 @@ public class SJF {
                 currentTime = completionTime[shortestJob];
             }
         }
-        
+
         double totalTurnAroundTime = 0;
         double totalWaitingTime = 0;
         System.out.println("\nHasil:");
-        System.out.println("Proses\tWaktu Kedatangan\tWaktu Burst\tWaktu Selesai\tWaktu Tunggu\tWaktu Putar");
+        System.out.format(
+                "+-----------------+-----------------------+------------------+-------------------+-------------------+-------------------+%n");
+        System.out.format("|%15s  | %21s | %16s | %17s | %17s | %17s |%n", "Proses", "Waktu Kedatangan", "Waktu Burst",
+                "Waktu Selesai", "Waktu Tunggu", "Waktu Putar");
+        System.out.format(
+                "+-----------------+-----------------------+------------------+-------------------+-------------------+-------------------+%n");
         for (int i = 0; i < n; i++) {
-            System.out.println((i+1) + "\t\t" + arrivalTime[i] + "\t\t\t" + burstTime[i] + "\t\t\t" + completionTime[i] + "\t\t\t" + waitingTime[i] + "\t\t\t" + turnAroundTime[i]);
+            System.out.format("|%15s  | %21s | %16s | %17s | %17s | %17s |%n", (i + 1), arrivalTime[i], burstTime[i],
+                    completionTime[i], waitingTime[i], turnAroundTime[i]);
             totalTurnAroundTime += turnAroundTime[i];
             totalWaitingTime += waitingTime[i];
         }
+        System.out.format(
+                "+-----------------+-----------------------+------------------+-------------------+-------------------+-------------------+%n");
         double averageTurnAroundTime = totalTurnAroundTime / n;
         double averageWaitingTime = totalWaitingTime / n;
-        System.out.println("\nWaktu rata-rata putar: " + averageTurnAroundTime);
-        System.out.println("Waktu rata-rata tunggu: " + averageWaitingTime);
+        System.out.format("%-17s %-25.2f %-19s %-18.2f %-18s %-18.2f%n", "Waktu rata-rata putar:",
+                averageTurnAroundTime, "\nWaktu rata-rata tunggu:", averageWaitingTime);
     }
 }
